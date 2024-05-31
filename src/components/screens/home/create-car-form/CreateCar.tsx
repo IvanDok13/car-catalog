5da830de617e85e93c786919db066e0a8bce1ab7
Ivan Klochkov <vany-nov13@yandex.ru>
2024-05-31 15:33:21 +0200

    feat: add react hook form

--- src/components/screens/home/create-car-form/CreateCar.tsx
+++ src/components/screens/home/create-car-form/CreateCar.tsx
@@ -1,56 +1,75 @@
-import { useState } from 'react'
+import { useForm } from 'react-hook-form'
 import styles from './CreateCarForm.module.css'
 
-const clearData = {
-	name: '',
-	price: '',
-	image: '',
-}
+// const clearData = {
+// 	name: '',
+// 	price: '',
+// 	image: '',
+// }
 const CreateCarForm = ({ setCars }) => {
-	const [data, setData] = useState(clearData)
+	// const [data, setData] = useState(clearData)
 
-	const createCar = e => {
-		e.preventDefault()
+	const {
+		register,
+		reset,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		mode: 'onChange',
+	})
 
+	// const createCar = e => {
+	// e.preventDefault()
+	const createCar = data => {
 		setCars(prev => [{ id: prev.length + 1, ...data }, ...prev])
 
-		setData(clearData)
+		reset()
 	}
 
 	return (
-		<form className={styles.form}>
+		<form className={styles.form} onSubmit={handleSubmit(createCar)}>
 			<input
+				// placeholder='Name'
+				// onChange={e =>
+				// 	setData(prev => ({
+				// 		...prev,
+				// 		name: e.target.value,
+				// 	}))
+				// }
+				// value={data.name}
+				{...register('name', { required: 'Name is required' })}
 				placeholder='Name'
-				onChange={e =>
-					setData(prev => ({
-						...prev,
-						name: e.target.value,
-					}))
-				}
-				value={data.name}
 			/>
+			{errors?.name?.message && (
+				<p style={{ color: 'red' }}>Name is required</p>
+			)}
 			<input
 				placeholder='Price'
-				onChange={e =>
-					setData(prev => ({
-						...prev,
-						price: e.target.value,
-					}))
-				}
-				value={data.price}
+				// onChange={e =>
+				// 	setData(prev => ({
+				// 		...prev,
+				// 		price: e.target.value,
+				// 	}))
+				// }
+				// value={data.price}
+				{...register('price', { required: true })}
 			/>
 			<input
 				placeholder='Image'
-				onChange={e =>
-					setData(prev => ({
-						...prev,
-						image: e.target.value,
-					}))
-				}
-				value={data.image}
+				// onChange={e =>
+				// 	setData(prev => ({
+				// 		...prev,
+				// 		image: e.target.value,
+				// 	}))
+				// }
+				// value={data.image}
+				{...register('image', { required: true })}
 			/>
 
-			<button className={styles.btn} onClick={e => createCar(e)}>
+			<button
+				className={styles.btn}
+				// onClick={e => createCar(e)}
+			>
 				Create
 			</button>
 		</form>
